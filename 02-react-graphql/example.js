0a5f49fe3516d4ddfd3789eb36886caa27aeda98
Brian Alejandro Ochoa Duran <brian16ochoa@gmail.com>
2023-08-03 18:44:24 -0600

    feat(main): changes saved on main

---
+++ 02-react-graphql/example.js
@@ -0,0 +1,25 @@
+let gloablaData = null;
+// fetch('google.com').then(result => { result.json()}).then(data => gloablaData = data).catch(e => {
+//     console.log(e)
+// }).finally(){
+    
+// }
+
+const getGoogle = async () =>{
+try{
+    let result = await fetch('google.com');
+    gloablaData = await result.json()
+}catch(error){
+    console.log(error);
+}finally{
+    console.log("Fetch done")
+}
+}
+class MyClass{
+    MyClass(number){
+        this.number = number
+    }
+}
+const newObject = new MyClass('123')
+
+newObject.number
---
+++ mensajes_app/src/main/java/org/example/Conexion.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    public Connection getConnection(){
+        Connection connection = null;
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mensajes_app","root","");
+            if(connection != null){
+                System.out.println("Conexion exitosa");
+            }
+        }catch (SQLException error){
+            System.out.println("Error: "+ error);
+        }
+        return connection;
+    }
+}
